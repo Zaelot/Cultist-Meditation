c1601d2f49c3b0e8a20070b9f84d704be69e27dc
stcl <jyrki.jakonen@gmail.com>
2016-01-30 17:40:34 +0200

    stuff from completed to project

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+    // Position from Map
+
+    private bool isAlive = true;
+    private int hitPoints = 2;
+    private int stuff = 0;
+    private int moveAmount = 100;
+
+
+
+	private Vector2 position;
+	// Use this for initialization
+	void Start () {
+		position = new Vector2 (transform.localPosition.x, transform.localPosition.y);
+	}
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Take collider attack ability
+        // Take hitpoints if enough
+        if( hitPoints <= 0 )
+            isAlive = false;
+
+        //Instantiate(explosionPrefab, pos, rot);
+        //Destroy(gameObject);
+
+        //killEnemy();
+        //Instantiate ( this.items )
+    }
+
+    void killEnemy() { }
+
+
+
+    // Update is called once per frame
+    void Update () {
+	
+   
+	}
+
+	public void MoveUp () {
+		position = new Vector2 (position.x, position.y + moveAmount);
+		transform.position = position;
+	}
+
+	public void MoveDown () {
+		position = new Vector2 (position.x, position.y - moveAmount);
+		transform.position = position;
+	}
+
+	public void MoveLeft () {
+		position = new Vector2 (position.x - moveAmount, position.y);
+		transform.position = position;
+	}
+
+	public void MoveRight () {
+		position = new Vector2 (position.x + moveAmount, position.y);
+		transform.position = position;
+	}
+
+	public void Action () {
+		
+	}
+}
--- Assets/Scripts/TwitchIRC-Unity/TwitchIrcListener.cs
+++ Assets/Scripts/TwitchIRC-Unity/TwitchIrcListener.cs
@@ -2,56 +2,65 @@
 using System.Collections;
 using System.Collections.Generic;
 
-[RequireComponent(typeof(TwitchIRC))]
-public class TwitchIrcListener : MonoBehaviour {
-	public string botName;
-	public GameObject cultistGO;
-	private Cultist cultist;
-	private TwitchIRC IRC;
-	//when message is recieved from IRC-server or our own message.
-	void OnChatMsgReceived(string msg)
-	{
-		//parse from buffer.
-		int msgIndex = msg.IndexOf("PRIVMSG #");
-		string msgString = msg.Substring(msgIndex + IRC.channelName.Length + 11);
-		string user = msg.Substring(1, msg.IndexOf('!') - 1);
 
-		Debug.Log (user + ":" + msgString);
-		if (user == botName) {
-			//add new message.
-			SendInputMessage(msgString);
-		}
-	}
-	void SendInputMessage(string msgString)
-	{
-		//TODO: Send input.
-		switch (msgString) {
-		case "Up":
-			cultist.MoveUp ();
-			break;
-		case "Down":
-			cultist.MoveDown ();
-			break;
-		case "Left":
-			cultist.MoveLeft ();
-			break;
-		case "Right":
-			cultist.MoveRight ();
-			break;
-		case "Action":
-			cultist.Action ();
-			break;
-		default:
-			Debug.Log ("Not a game command.");
-			break;
-		}
-	}
-	// Use this for initialization
-	void Start()
-	{
-		IRC = this.GetComponent<TwitchIRC>();
-		cultist = cultistGO.GetComponent<Cultist>();
-		//IRC.SendCommand("CAP REQ :twitch.tv/tags"); //register for additional data such as emote-ids, name color etc.
-		IRC.messageRecievedEvent.AddListener(OnChatMsgReceived);
-	}
+
+namespace Cultist
+{
+
+    [RequireComponent(typeof(TwitchIRC))]
+    public class TwitchIrcListener : MonoBehaviour
+    {
+        public string botName;
+        public GameObject playerGO;
+        private Player player;
+        private TwitchIRC IRC;
+        //when message is recieved from IRC-server or our own message.
+        void OnChatMsgReceived(string msg)
+        {
+            //parse from buffer.
+            int msgIndex = msg.IndexOf("PRIVMSG #");
+            string msgString = msg.Substring(msgIndex + IRC.channelName.Length + 11);
+            string user = msg.Substring(1, msg.IndexOf('!') - 1);
+
+            Debug.Log(user + ":" + msgString);
+            if (user == botName)
+            {
+                //add new message.
+                SendInputMessage(msgString);
+            }
+        }
+        void SendInputMessage(string msgString)
+        {
+            //TODO: Send input.
+            switch (msgString)
+            {
+                case "Up":
+                    player.MoveUp();
+                    break;
+                case "Down":
+                    player.MoveDown();
+                    break;
+                case "Left":
+                    player.MoveLeft();
+                    break;
+                case "Right":
+                    player.MoveRight();
+                    break;
+                case "Action":
+                    player.Action();
+                    break;
+                default:
+                    Debug.Log("Not a game command.");
+                    break;
+            }
+        }
+        // Use this for initialization
+        void Start()
+        {
+            IRC = this.GetComponent<TwitchIRC>();
+            player = playerGO.GetComponent<Player>();
+            //IRC.SendCommand("CAP REQ :twitch.tv/tags"); //register for additional data such as emote-ids, name color etc.
+            IRC.messageRecievedEvent.AddListener(OnChatMsgReceived);
+        }
+    }
 }
