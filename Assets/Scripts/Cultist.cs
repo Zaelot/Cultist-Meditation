e64299a304f78b6b1b9d9f029a2415566e6727e5
LasseHall <lasse.hall@bitbar.com>
2016-01-30 02:35:10 +0200

    Twitch chat user can now send messages to the Game

---
+++ Assets/Scripts/Cultist.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cultist : MonoBehaviour {
+
+	public float moveAmount = 100;
+	private Vector2 position;
+	// Use this for initialization
+	void Start () {
+		position = new Vector2 (transform.localPosition.x, transform.localPosition.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void MoveUp () {
+		position = new Vector2 (position.x, position.y + moveAmount);
+		transform.position = position;
+	}
+
+	public void MoveDown () {
+		position = new Vector2 (position.x, position.y - moveAmount);
+		transform.position = position;
+	}
+
+	public void MoveLeft () {
+		position = new Vector2 (position.x - moveAmount, position.y);
+		transform.position = position;
+	}
+
+	public void MoveRight () {
+		position = new Vector2 (position.x + moveAmount, position.y);
+		transform.position = position;
+	}
+
+	public void Action () {
+		
+	}
+}
---
+++ Assets/Scripts/TwitchIRC-Unity/TwitchIrcListener.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(TwitchIRC))]
+public class TwitchIrcListener : MonoBehaviour {
+	public string botName;
+	public GameObject cultistGO;
+	private Cultist cultist;
+	private TwitchIRC IRC;
+	//when message is recieved from IRC-server or our own message.
+	void OnChatMsgReceived(string msg)
+	{
+		//parse from buffer.
+		int msgIndex = msg.IndexOf("PRIVMSG #");
+		string msgString = msg.Substring(msgIndex + IRC.channelName.Length + 11);
+		string user = msg.Substring(1, msg.IndexOf('!') - 1);
+
+		if (user == botName) {
+			//add new message.
+			SendInputMessage(msgString);
+		}
+	}
+	void SendInputMessage(string msgString)
+	{
+		//TODO: Send input.
+		switch (msgString) {
+		case "Up":
+			cultist.MoveUp ();
+			break;
+		case "Down":
+			cultist.MoveDown ();
+			break;
+		case "Left":
+			cultist.MoveLeft ();
+			break;
+		case "Right":
+			cultist.MoveRight ();
+			break;
+		case "Action":
+			cultist.Action ();
+			break;
+		default:
+			Debug.Log ("Not a game command.");
+			break;
+		}
+	}
+	// Use this for initialization
+	void Start()
+	{
+		IRC = this.GetComponent<TwitchIRC>();
+		cultist = cultistGO.GetComponent<Cultist>();
+		//IRC.SendCommand("CAP REQ :twitch.tv/tags"); //register for additional data such as emote-ids, name color etc.
+		IRC.messageRecievedEvent.AddListener(OnChatMsgReceived);
+	}
+}
